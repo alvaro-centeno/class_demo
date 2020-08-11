d76a6da4964a6a003554bdd6a122196f3581b63d
Alvaro Centeno <alvaroc0689@gmail.com>
2020-08-10 19:36:43 -0700

    Updates

---
+++ precache-manifest.4872ecd792f126f6b73dabce09b6590d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb0433a619d64c83a183b94535676988",
+    "url": "/class_demo/index.html"
+  },
+  {
+    "revision": "6fab3e372b8298e8b036",
+    "url": "/class_demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "db7a627cbc2b935499d5",
+    "url": "/class_demo/static/js/2.a925df73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/class_demo/static/js/2.a925df73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fab3e372b8298e8b036",
+    "url": "/class_demo/static/js/main.b6372269.chunk.js"
+  },
+  {
+    "revision": "f0e9045c427eef62729e",
+    "url": "/class_demo/static/js/runtime-main.86c35918.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/class_demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
